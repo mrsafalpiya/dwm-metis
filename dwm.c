ff28fe4c7a3bda69701c1060c0e7940c014c23e8
Safal Piya <mrsafalpiya@gmail.com>
2022-03-01 19:16:57 +0545

    Modified status color
    Now status follows the colorscheme of the title section

--- dwm.c
+++ dwm.c
@@ -710,7 +710,10 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
+		if (m->sel)
+			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+		else
+			drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
 	}
